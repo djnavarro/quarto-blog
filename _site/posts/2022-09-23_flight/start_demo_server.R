fd657642a5099eded73206e3ff884b62ec6ad25b
Danielle Navarro <djnavarro@protonmail.com>
2022-09-28 11:20:26 +1000

    tidies server-in-use examples

--- _site/posts/2022-09-23_flight/start_demo_server.R
+++ _site/posts/2022-09-23_flight/start_demo_server.R
@@ -1,10 +1,11 @@
 
+## ---- start-demo-server --------
 # load R packages and specify the Python environment
 library(arrow)
 library(reticulate)
 use_miniconda("base")
 
-# load the server class, create an instance, and start serving
+# load server class, create instance, start serving
 server_class <- load_flight_server("demo_flight_server")
 server <- server_class$DemoFlightServer(port = 8089)
 server$serve()
